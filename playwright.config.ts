0378c1561cc2e14d30493af5614bf13f420a129b
Corey Alix <calix@dataworksplus.com>
2026-02-06 11:54:36 -0500

    feat: update Playwright configuration to emulate dark mode by default

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,6 +30,9 @@ export default defineConfig({
 
         /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
         trace: 'on-first-retry',
+
+        /* Emulate dark mode by default */
+        colorScheme: 'dark',
     },
 
     /* Configure projects for major browsers */
